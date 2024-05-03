c85c974c03997e9306d7e6ad02579d7e6d4c61ea
ibrahim3761 <mdibrahimabdullah1@gmail.com>
2024-05-03 23:20:30 +0600

    D_1_Reverse_Card_Easy_Version.cpp

--- D_1_Reverse_Card_Easy_Version.cpp
+++ D_1_Reverse_Card_Easy_Version.cpp
@@ -1,40 +1,24 @@
-/* Mohammad Ibrahim Abdullah*/
+
 #include <bits/stdc++.h>
-#define N 100005
-#define yes cout << "YES" << endl;
-#define no cout << "NO" << endl;
-#define ll long long
-#define ull unsigned long long
-#define cint cin >> t;
 using namespace std;
+typedef long long ll;
+const int N = 2000005;
+int tc, n, m;
+ll ans;
+inline void solve()
+{
+    cin >> n >> m;
+    ans = 0;
+    for (int i = 1; i <= m; i++)
+        ans += (n + i) / (1ll * i * i);
+    cout << ans - 1 << '\n';
+}
 int main()
 {
-    int t;
-    cint while (t--)
-    {
-        int i, count = 0, n, m, a=1, b=1;
-        cin >> n >> m;
-        string s;
-        cin >> s;
-        while (a < n+1)
-        {
-            b = 1;
-            while (b < m+1)
-            {
-                if ((a + b) % (b * __gcd(a, b)) == 0)
-                {
-                    count++;
-                    b++;
-                }
-                else
-                {
-                    b++;
-                } 
-            }
-            a++;
-        }
-
-        cout << count << endl;
-    }
+    cin.tie(0);
+    cout.tie(0);
+    cin >> tc;
+    while (tc--)
+        solve();
     return 0;
 }
