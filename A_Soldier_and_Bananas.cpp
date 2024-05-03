d247b28e0974d6094502e68154b8739c5bb2c2c6
ibrahim3761 <mdibrahimabdullah1@gmail.com>
2024-05-03 23:19:14 +0600

    A_Soldier_and_Bananas.cpp

---
+++ A_Soldier_and_Bananas.cpp
@@ -0,0 +1,34 @@
+/* Mohammad Ibrahim Abdullah*/
+#include <bits/stdc++.h>
+#define N 100005
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define ll long long
+#define ull unsigned long long
+#define cint cin >> t;
+using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t = 1;
+    // cin>>t;
+    while (t--)
+    {
+        int k, n, w, i, sum = 0;
+        cin >> k >> n >> w;
+        for (i = 1; i <= w; i++)
+        {
+            sum += k * i;
+        }
+        if (sum > n)
+        {
+            cout << sum - n << endl;
+        }
+        else
+        {
+            cout << '0' << endl;
+        }
+    }
+    return 0;
+}
